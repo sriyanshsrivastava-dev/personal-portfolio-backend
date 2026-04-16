88b3a27cce37992dd5c487bc7a8d255763a39d36
Sriyansh Srivastava <sriyanshsrivastava.dev@gmail.com>
2026-04-17 01:40:08 +0530

    chore: add Project Folder Structure.

---
+++ src/main/java/com/sriyanshsrivastava/personalportfoliobackend/controller/APIHealth.java
@@ -0,0 +1,12 @@
+package com.sriyanshsrivastava.personalportfoliobackend.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIHealth {
+
+    @GetMapping("/health")
+    public String apiHealth(){
+        return "OK";
+    }
+}
---
+++ src/main/java/com/sriyanshsrivastava/personalportfoliobackend/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.sriyanshsrivastava.personalportfoliobackend.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello world!";
+    }
+
+}
