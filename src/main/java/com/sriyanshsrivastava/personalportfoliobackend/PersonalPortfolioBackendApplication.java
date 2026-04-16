4eb42111e930ca6563e4237a8b21f67a495a3503
Sriyansh Srivastava <sriyanshsrivastava.dev@gmail.com>
2026-04-17 01:09:45 +0530

    chore: add initial project structure.

---
+++ src/main/java/com/sriyanshsrivastava/personalportfoliobackend/PersonalPortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sriyanshsrivastava.personalportfoliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPortfolioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalPortfolioBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sriyanshsrivastava/personalportfoliobackend/PersonalPortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriyanshsrivastava.personalportfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPortfolioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
